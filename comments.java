9b214e538164a437162824cd588425434ab41dbc
calkia <kcy.1288@gmail.com>
2025-02-11 10:07:00 -0500

    Update comments.java

---
+++ comments.java
@@ -0,0 +1,16 @@
+/******************************
+ * Project: Demo For Comments
+ * Programmer: Kiana Tjon
+ * Date: 2/11/25
+ * Program Name: comments.java
+ ******************************/
+
+public class comments {
+    
+    public static void main(String[]args)
+    {
+        System.out.println("Hi"); //Message to user
+        System.out.println("LabOneA"); //assignment
+    }
+/*Block style coding for detailed comments */
+}
