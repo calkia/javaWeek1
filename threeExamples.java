0a44970f2ff01c2e4d853b11d7a60386a41565ef
calkia <kcy.1288@gmail.com>
2025-02-11 10:26:58 -0500

    Create threeExamples.java

---
+++ threeExamples.java
@@ -0,0 +1,10 @@
+public class threeExamples {
+    
+    public static void main(String[]args)
+    {
+        //Hello
+        //System.out.println("\n\tHello\\\'");
+        //Dog Show Winners
+        System.out.println("\t\t\tDog Show Winners\n\nPlace:\t1\t2\t2\t3\t4\t5\t6\nDog #:");
+    }
+}
