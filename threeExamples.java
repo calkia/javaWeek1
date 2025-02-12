eda240fafc0a51477370709cbe8e42a74bb50b40
calkia <kcy.1288@gmail.com>
2025-02-12 10:08:48 -0500

    Update threeExamples.java

--- threeExamples.java
+++ threeExamples.java
@@ -5,6 +5,12 @@ public class threeExamples {
         //Hello
         //System.out.println("\n\tHello\\\'");
         //Dog Show Winners
-        System.out.println("\t\t\tDog Show Winners\n\nPlace:\t1\t2\t2\t3\t4\t5\t6\nDog #:");
+        System.out.println("\t\t\tDog Show Winners\n\nPlace:\t1\t2\t3\t4\t5\t6\nDog #:\t76\t32\t18\t47\t11\t29");
+        System.out.println();
+        //secret coded message
+        System.out.println("Q\"yz89\"Ja4\"tp");
+        System.out.println();
+        //locker numbers
+        System.out.println("\tAnderson\t3452\n\tBoyton\t\t5344\n\tCarrington\t3677\n\tHouston\t\t5988\n\tMcDougal\t5233\n\tRobertson\t3623\n\tStryker\t\t5112");
     }
 }
